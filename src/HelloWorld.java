571ea99de3b5f261bf82e22e43be9cd5f86bc003
jules-testo <summer.julia@outlook.at>
2021-03-04 11:21:02 +0100

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,23 @@
+
+// Kommentar
+/*
+    1. Klassenname muss Dateiname entsprechen
+    2. Ändern => Refactor (SHIFT + F6)
+ */
+public class HelloWorld {
+
+    //3. psvm+Enter generiert uns die main-Methode
+    public static void main(String[] args) {
+        // Startpunkt für unsere Programme
+
+        System.out.println("Hello World on day 4321");
+        //Liebes "System"
+        // wir owolen etwar "out" ausgeben
+        // print
+        // ln (neue Zeitle)
+        // übergeben mit ".." einen Text
+        // Abk. "sout"+Enter 0 System.out.println();
+
+    }
+}
+
