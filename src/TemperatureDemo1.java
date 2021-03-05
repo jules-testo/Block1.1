1c9e32343243cb9eceff64f0a55c12ea66ea1561
jules-testo <summer.julia@outlook.at>
2021-03-05 10:32:41 +0100

    all done

---
+++ src/TemperatureDemo1.java
@@ -0,0 +1,22 @@
+public class TemperatureDemo1 {
+    public static void main(String[] args) {
+
+        double temperatureCelsius = 11.8;
+        double temperatureCelsius1 = 4.8;
+
+        double fahrenheit = celsiusToFahrenheit(temperatureCelsius);
+        double fahrenheit1 = celsiusToFahrenheit(temperatureCelsius1);
+
+        System.out.println("Aktuelle Temperatur 1 " + temperatureCelsius + " in Celsius entspricht " + fahrenheit + " in Fahrenheit");
+
+
+    }
+
+    public static double celsiusToFahrenheit (double celsius) {
+
+        double result = celsius * 1.8 + 32;
+        return result;
+    }
+}
+{
+}
